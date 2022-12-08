e64221c656621bf907b96dd83831730a3ff33223
morita <morita31ssgb@gmail.com>
2022-12-09 00:18:13 +0900

    Revert "Add:userテーブルにgenderカラムを追加"
    This reverts commit 33297065e9c424dc9cda1c039c94cab99dad8a59.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_04_154421) do
+ActiveRecord::Schema.define(version: 2022_01_19_072358) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -135,7 +135,6 @@ ActiveRecord::Schema.define(version: 2022_12_04_154421) do
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "gender", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
